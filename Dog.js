f49faace12dba07725ed9b0ed67367b57fe4332c
DAIPHONG <tranvantuananh221298@gmail.com>
2020-04-25 20:11:23 -0700

    sayHi method for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,17 @@
-function Dog(){
+var chalk = require('chalk');
+// dùng modules chalk 
+function Dog(name){
+	this.name = name;
 	this.stomach =[];
 	
 }
 Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
 };
+Dog.prototype.sayHi = function() {
+	console.log('Hi! I am a dog. My name is' + chalk.rep(this.name));
+	//gọi modules chalk này ra, màu sắc đỏ
+};
+
+module.exports = Dog;
+//exports thằng này ra
